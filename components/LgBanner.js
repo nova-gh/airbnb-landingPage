dfb2e40f2a44b50bd1f1e7309ea9377d4fe8b1d2
Nova Gh <nghimire.dev@gmail.com>
2021-08-05 14:54:35 -0700

    Update LgBanner.js

--- components/LgBanner.js
+++ components/LgBanner.js
@@ -1,5 +1,14 @@
 import Image from "next/image";
-const LgBanner = ({ img, title, description, buttonText, alt }) => {
+const LgBanner = ({
+  img,
+  title,
+  description,
+  buttonText,
+  alt,
+  buttonColor,
+  textColor,
+  buttonTextColor,
+}) => {
   return (
     <div className="relative ">
       <div className="relative h-96 min-w-[300px]">
@@ -12,9 +21,13 @@ const LgBanner = ({ img, title, description, buttonText, alt }) => {
         />
       </div>
       <div className="absolute space-y-2 top-32 left-12">
-        <h3 className="w-64 mb-3 text-4xl font-medium">{title}</h3>
-        <p className="">{description}</p>
-        <button className="px-4 py-2 text-white bg-gray-900 rounded-xl">
+        <h3 className={`w-64 mb-3 text-4xl font-medium bg ${textColor} `}>
+          {title}
+        </h3>
+        <p className={`max-w-xs ${textColor}`}>{description}</p>
+        <button
+          className={`px-4 py-2 rounded-xl  ${buttonTextColor}  ${buttonColor}`}
+        >
           {buttonText}
         </button>
       </div>
