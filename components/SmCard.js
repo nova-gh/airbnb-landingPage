5afaa260a658833585514b39a972a20f90a82702
Nova Gh <nghimire.dev@gmail.com>
2021-08-04 23:11:32 -0700

    Update SmCard.js

--- components/SmCard.js
+++ components/SmCard.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const SmCard = ({ img, location, distance }) => {
   return (
-    <div className="flex items-center my-2 space-x-4 transition duration-150 ease-out cursor-pointer rounded-xl hover:bg-gray-100 hover:scale-105 max-w-max">
+    <div className="flex items-center my-2 space-x-4 transition duration-150 ease-out transform cursor-pointer rounded-xl hover:bg-gray-100 hover:scale-105 max-w-max">
       <div className="relative w-[64px] h-[64px] ">
         <Image src={img} layout="fill" alt={location} className="rounded-lg" />
       </div>
