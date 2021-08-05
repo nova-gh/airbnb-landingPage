af5efc9158b8659ca4a68231ab7d12d0abe68dc7
Nova Gh <nghimire.dev@gmail.com>
2021-08-04 23:11:28 -0700

    Create MdCard.js

---
+++ components/MdCard.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+const MdCard = ({ img, title }) => {
+  return (
+    <div className="transition duration-300 ease-out transform cursor-pointer max-w-max hover:bg-gray-100 hover:scale-105">
+      <div className="relative h-[272px] w-[272px] md:h-[216px] md:w-[216px] lg:w-[302px] lg:h-[302px] xl:w-[307px] xl:h-[307px]">
+        <Image src={img} layout="fill" alt={title} className="rounded-lg" />
+      </div>
+      <div className="">
+        <h2 className="text-lg font-semibold">{title}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default MdCard;
