c017542d92dff7da312b0ab49b189269dc7fda17
Nova Gh <nghimire.dev@gmail.com>
2021-08-04 20:04:12 -0700

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { FaAirbnb, FaSearch, FaUserCircle } from "react-icons/fa";
+import { FiGlobe, FiMenu } from "react-icons/fi";
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10 ">
+      {/* left */}
+      <div className="flex items-center cursor-pointer text-brand">
+        <FaAirbnb className="text-4xl" />
+        <h1 className="text-xl font-bold">airbnb</h1>
+      </div>
+      {/* middle */}
+      <div className="flex items-center p-2 transition duration-100 ease-in-out delay-100 rounded-full md:shadow-sm md:border-2 md:hover:shadow-md">
+        <input
+          type="text"
+          placeholder="Start your search"
+          className="flex-grow pl-5 font-medium bg-transparent outline-none text-primeBlack placeholder-primeBlack"
+        />
+        <FaSearch className="hidden p-2 text-3xl text-white rounded-full cursor-pointer md:inline bg-brand" />
+      </div>
+      {/* right */}
+      <div className="flex items-center ml-auto space-x-4 text-primeBlack">
+        <Link href="/">
+          <a className="hidden md:inline">Become a host</a>
+        </Link>
+        <FiGlobe className="text-lg cursor-pointer" />
+        <div className="flex items-center p-2 space-x-2 border-2 rounded-full shadow-sm cursor-pointer hover:shadow-md">
+          <FiMenu className="text-gray-500" />
+          <FaUserCircle className="text-2xl text-gray-500" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
