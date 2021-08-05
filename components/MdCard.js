63ae43c7ae503ecceaee6a508242766feadb13af
Nova Gh <nghimire.dev@gmail.com>
2021-08-05 00:10:48 -0700

    Update MdCard.js

--- components/MdCard.js
+++ components/MdCard.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-const MdCard = ({ img, title }) => {
+const MdCard = ({ img, title, description }) => {
   return (
     <div className="my-2 space-y-1 transition duration-300 ease-out transform cursor-pointer max-w-max hover:scale-105 rounded-xl">
       <div className="relative h-[272px] w-[272px] md:h-[216px] md:w-[216px] lg:w-[302px] lg:h-[302px] xl:w-[307px] xl:h-[307px]">
@@ -7,6 +7,7 @@ const MdCard = ({ img, title }) => {
       </div>
       <div className="">
         <h2 className="text-lg font-semibold">{title}</h2>
+        <p className="text-sm">{description}</p>
       </div>
     </div>
   );
