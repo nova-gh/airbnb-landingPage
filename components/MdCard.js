c937fd5bbb796b07ef9131f4efd569a534f2bae2
Nova Gh <nghimire.dev@gmail.com>
2021-08-04 23:44:34 -0700

    Update MdCard.js

--- components/MdCard.js
+++ components/MdCard.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const MdCard = ({ img, title }) => {
   return (
-    <div className="transition duration-300 ease-out transform cursor-pointer max-w-max hover:bg-gray-100 hover:scale-105">
+    <div className="my-2 space-y-1 transition duration-300 ease-out transform cursor-pointer max-w-max hover:scale-105 rounded-xl">
       <div className="relative h-[272px] w-[272px] md:h-[216px] md:w-[216px] lg:w-[302px] lg:h-[302px] xl:w-[307px] xl:h-[307px]">
         <Image src={img} layout="fill" alt={title} className="rounded-lg" />
       </div>
